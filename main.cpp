dffb2daf253b5544201334f3ee0623f6609fd484
= <=>
2023-09-29 09:49:30 +0800

    Implement basic login prototype

--- main.cpp
+++ main.cpp
@@ -2,9 +2,40 @@
 
 using namespace std;
 
+// Basic authorization prototype
+bool authorize (string username, string password)
+{
+  // Using preset value to mock functionality
+  if (username == "khilfi" && password == "1234")
+  {
+    return true;
+  }
+  return false;
+}
+
+// This is where program starts executing
 int main ()
 {
-  cout << "Hello, world!";
+  cout << "Welcome to INTEC Student Management System" << endl;
+  
+  while (1)
+  {
+    cout << "Please enter your username and password to login." << endl;
 
-  return 0;
+    string usernameIn, passwordIn;
+
+    cout << "Username: ";
+    cin >> usernameIn;
+    cout << "Password: ";
+    cin >> passwordIn;
+
+    if (!authorize(usernameIn, passwordIn))
+    {
+      cout << "Invalid username or password" << endl;
+      continue;
+    }
+
+    cout << "Welcome " << usernameIn << "!";
+    return 0;
+  }
 }
